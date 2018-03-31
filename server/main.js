82f1108dab1c5dd60e131ab312b29bdc8d4b2d7f
Sejin Jeon <jinaidy93@gmail.com>
2018-03-31 17:47:17 +0900

    Change build directory structure
    Directory named 'build' was a directory for transpiled server codes.
    In previous, utils are inside of server directory and I didn't used
    modules of utils in server side, so the problem didn't occured.
    But as I separate two directories and use isEmpty of nullChecker.js, I had
    to change the build directory structure.
    
    I made build/server and build/utils.
    And because of this, I had to modify the relative path of 'public' path in
    server/main.js.

--- server/main.js
+++ server/main.js
@@ -6,15 +6,15 @@ import path from 'path';
 
 const app = express();
 const port = process.env.SERVER_ENV === 'development' ? 5913 : 80;
+const publicPath = path.resolve(__dirname, "../../public");   // Directory would be deeper one step when it is transpiled.
 
-app.use(express.static(__dirname + "/../public"));
+app.use(express.static(publicPath));
 app.use('/posts', posts);
 app.use('/post', post);
 app.use('/tags', tags);
 app.get('/*', (req,res) => {
-    res.sendFile(path.resolve(__dirname + "/../public/index.html"));
+    res.sendFile(path.resolve(publicPath, "index.html"));
 });
-console.log(__dirname + "/../public/index.html");
 
 app.listen(port, () => {
     console.log('Express listening on port', port);
