76a02ec9f27602fce1cdd15acdaeb6da2318ecb6
Sejin Jeon <jinaidy93@gmail.com>
2018-07-06 21:27:38 +0900

    Change font loading system

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 loader: 'style-loader!css-loader'
             },
             {
-                test: /\.png$|\.jpg$/,
+                test: /\.png$|\.jpg$|\.ttf$/,
                 loader: 'url-loader'
             }
         ]
