9d44b7ec722337471094cf36213e38a8b5a945c8
Sejin Jeon <jinaidy93@gmail.com>
2019-03-14 14:44:58 +0900

    Create the config file and add connection info

--- server/db/models.js
+++ server/db/models.js
@@ -1,25 +1,28 @@
-import connectToDB, {mongoose} from './dbConnection'
+import mongoose from 'mongoose';
+import { loadConnectionInfo, makeConnection } from './makeConnection';
 
-connectToDB('172.18.0.3', '172.18.0.2', 27017, 'blog');
+const serverEnv = process.argv[2];
+const { addressList, port, database } = loadConnectionInfo(serverEnv);
+makeConnection(addressList, port, database);
 
 const post = new mongoose.Schema({
-    postNo: {type: Number, required: true, unique: true},
-    title: {type: String, required: true, unique: true},    // unique: mdFile cannot have unique keys like hash or postNo...
-    dateCreated: {type: Date, required: true, unique: false},
-    dateUpdated: {type: Date, required: true, unique: false},
-    content: {type: String, required: false, unique: false},
-    tags: {type: Array, required: false, unique: false},    // just a string array composed of tagNames
-    belongToMajor: {type: String, required: true, unique: false},
-    belongToMinor: {type: String, required: true, unique: false},
+  postNo: { type: Number, required: true, unique: true },
+  title: { type: String, required: true, unique: true }, // unique: mdFile cannot have unique keys like hash or postNo...
+  dateCreated: { type: Date, required: true, unique: false },
+  dateUpdated: { type: Date, required: true, unique: false },
+  content: { type: String, required: false, unique: false },
+  tags: { type: Array, required: false, unique: false }, // just a string array composed of tagNames
+  belongToMajor: { type: String, required: true, unique: false },
+  belongToMinor: { type: String, required: true, unique: false },
 });
 
 const tag = new mongoose.Schema({
-    tagName: {type: String, required: true, unique: true},
-    belongToMinor: {type: String, required: true, unique: false},
-    postList: {type: Array, required: true, unique: false},
+  tagName: { type: String, required: true, unique: true },
+  belongToMinor: { type: String, required: true, unique: false },
+  postList: { type: Array, required: true, unique: false },
 });
 
-const Post = mongoose.model("Post", post, "posts");
-const Tag = mongoose.model("Tag", tag, "tags");
+const Post = mongoose.model('Post', post, 'posts');
+const Tag = mongoose.model('Tag', tag, 'tags');
 
 export { Post, Tag };
