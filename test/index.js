9846dd3b50345c4de1f286e65c6e8a711b0b06f9
Sejin Jeon <jinaidy93@gmail.com>
2019-04-07 16:49:33 +0900

    Change the location of before & after for MongoServerMock

--- test/index.js
+++ test/index.js
@@ -1,5 +1,23 @@
 import sinon from 'sinon';
-import testDao from './testMongoDB';
+import mongoose from 'mongoose';
+import MongoMemoryServer from 'mongodb-memory-server';
+import testMongoDB from './testMongoDB';
+import testScripts from './server/testScripts';
+import connectToMock from "./connectToMock";
+
+const mongoServerMock = new MongoMemoryServer();
+
+before(async () => {
+  console.log('[+] preprocessing...');
+  await connectToMock(mongoServerMock);
+});
+
+after(() => {
+  console.log('[+] Test is all over. Clearing the mongoServerMock...');
+  mongoose.disconnect();
+  mongoServerMock.stop();
+});
 
 sinon.stub(console, 'error'); // Suppress error message from catch phrases
-testDao();
+testMongoDB(mongoServerMock);
+testScripts(mongoServerMock);
--- test/testMongoDB.js
+++ test/testMongoDB.js
@@ -1,41 +1,8 @@
-import mongoose from 'mongoose';
-import Bluebird from 'bluebird';
-import MongoMemoryServer from 'mongodb-memory-server';
-import Post from '../server/mongoDB/models/Post';
-import Category from '../server/mongoDB/models/Category';
-import Series from '../server/mongoDB/models/Series';
-import Tag from '../server/mongoDB/models/Tag';
 import testPostDao from './server/mongoDB/dao/testPostDao';
-import testTagDao from './server/mongoDB/dao/testTagDao';
-
-const modelsMock = {};
-const mongoServer = new MongoMemoryServer();
-const option = { useMongoClient: true };
 
 export default () => {
   describe('MongoDB', () => {
-    before((done) => {
-      console.log('[+] preprocessing...');
-      mongoose.Promise = Bluebird;
-      mongoServer
-        .getConnectionString()
-        .then(dbURL => mongoose.connect(dbURL, option))
-        .then(() => {
-          modelsMock.Post = Post;
-          modelsMock.Category = Category;
-          modelsMock.Series = Series;
-          modelsMock.Tag = Tag;
-          done();
-        }, err => done(err));
-    });
-
     testPostDao();
     // testTagDao();
-
-    after(() => {
-      console.log('[+] MongoDB test is over. Stopping the mongoServer mock...');
-      mongoose.disconnect();
-      mongoServer.stop();
-    });
   });
 };
