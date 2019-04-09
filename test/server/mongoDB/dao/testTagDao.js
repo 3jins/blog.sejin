1b0fff1f62e7a6fe5fc5846ba0d106703a167819
Sejin Jeon <jinaidy93@gmail.com>
2019-04-10 01:22:35 +0900

    Add a test for TagDao.upsertTag

---
+++ test/server/mongoDB/dao/testTagDao.js
@@ -0,0 +1,34 @@
+import * as TagDao from '../../../../server/mongoDB/dao/TagDao';
+
+export default () => {
+  describe('TagDao', () => {
+    describe('upsertTag', () => {
+      it('inserts a same tag multiple times with different posts and same belongToMinor', async () => {
+        const tagObjList = [
+          {
+            tagName: 'test tag',
+            postList: ['post1'],
+            belongToMinorList: ['tech'],
+          }, {
+            tagName: 'test tag',
+            postList: ['post2'],
+            belongToMinorList: ['tech'],
+          }, {
+            tagName: 'test tag',
+            postList: ['post3'],
+            belongToMinorList: ['tech'],
+          },
+        ];
+
+        await tagObjList.reduce(async (lastTagObj, tagObj) => {
+          await lastTagObj;
+          return TagDao.upsertTag(tagObj);
+        }, null);
+        const tag = (await TagDao.findTag({ tagName: 'test tag' }))[0];
+        console.log(tag);
+        tag.postList.should.have.lengthOf(tagObjList.length);
+        tag.belongToMinorList.should.have.lengthOf(1);
+      });
+    });
+  });
+};
--- test/testMongoDB.js
+++ test/testMongoDB.js
@@ -1,8 +1,9 @@
 import testPostDao from './server/mongoDB/dao/testPostDao';
+import testTagDao from './server/mongoDB/dao/testTagDao';
 
 export default () => {
   describe('MongoDB', () => {
     testPostDao();
-    // testTagDao();
+    testTagDao();
   });
 };
