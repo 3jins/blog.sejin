a63bfaadefa396d852385a89b10b2ce56ef7da9b
Sejin Jeon <jinaidy93@gmail.com>
2019-04-13 20:29:38 +0900

    Remove Facebook reply to fix a bug
    - Facebook reply is malfunctioning after updating npm packages. Since
      planning to change the reply system, just removed it to fix bug.
    - Refactored a little

--- client/components/content/post-viewer/ContentView.js
+++ client/components/content/post-viewer/ContentView.js
@@ -1,114 +1,136 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet/es/Helmet';
+import { connect } from 'react-redux';
 import * as actions from '../../../actions';
 import LoadingView from '../LoadingView';
-import ContentViewContent from "./ContentViewContent";
-import ContentViewSubtitle from "./ContentViewSubtitle";
-import {blogTitle, menuList} from "../../../constants";
-import {isEmpty} from "../../../../utils/nullChecker";
-import Helmet from "react-helmet/es/Helmet";
-import NotFoundView from "../NotFoundView";
+import ContentViewContent from './ContentViewContent';
+import ContentViewSubtitle from './ContentViewSubtitle';
+import { blogTitle, menuList } from '../../../constants';
+import NotFoundView from '../NotFoundView';
 
 class ContentView extends Component {
-    constructor(props) {
-        super(props);
-        this.menuList = menuList;
-        props.handleFetchPost('/post', props.postNo);
-    }
+  constructor(props) {
+    super(props);
+    this.postNo = props.postNo;
+    props.handleFetchPost('/post', props.postNo);
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (!isEmpty(nextProps.post)) {
-            this.props.handleChangeMenu(nextProps.post, this.menuList);
-        }
-        if (nextProps.menuActionType === 'CHANGE_MENU') {
-            this.props.handleChangeMenuFinished();
-        }
+  componentWillReceiveProps(nextProps) {
+    if (!_.isEmpty(nextProps.postPayload)) {
+      this.props.handleChangeMenu(nextProps.postPayload.post[0], menuList);
     }
-
-    shouldComponentUpdate(nextProps) {
-        return nextProps.isLoaded;
+    if (nextProps.menuActionType === 'CHANGE_MENU') {
+      this.props.handleChangeMenuFinished();
     }
+  }
 
-    render() {
-        const getBelongToMajor = (post) => {
-            if (!isEmpty(post)) {
-                return post[0].belongToMajor;
-            }
-            return '';
-        };
-        const getBelongToMinor = (post) => {
-            if (!isEmpty(post)) {
-                return post[0].belongToMinor;
-            }
-            return '';
-        };
-        const belongToMajor = getBelongToMajor(this.props.post);
-        const belongToMinor = getBelongToMinor(this.props.post);
-        let currentTags = [];
-        if (!isEmpty(this.props.post)) {
-            currentTags = this.props.post[0].tags;
-        }
+  shouldComponentUpdate(nextProps) {
+    return nextProps.isLoaded;
+  }
 
-        return (
-            <div className="content">
-                <Helmet>
-                    <meta property="og:url" content={"https://enhanced.kr/contentviewer/" + this.props.postNo}/>
-                    <title>
-                        {!isEmpty(this.props.post) && this.props.post[0].title + " :: " + blogTitle}
-                    </title>
-                </Helmet>
-                {!this.props.isLoaded && /* loading */
-                    <LoadingView/>
-                }
-                {this.props.isLoaded && isEmpty(this.props.post) && /* not found */
-                    <NotFoundView isFail={true}/>
-                }
-                {this.props.isLoaded && !isEmpty(this.props.post) && /* render */
-                <div className="content-body">
-                    {belongToMajor !== 'Works' &&
-                    <ContentViewSubtitle
-                        tagPayload={this.props.tagPayload}
-                        currentTags={currentTags}
-                        belongToMinor={belongToMinor}
-                    />}
-                    <div className={"content-view-wrapper"}>
-                        <ContentViewContent
-                            post={this.props.post}
-                            belongToMajor={belongToMajor}
-                            postNo={this.props.postNo}
-                            tags={this.props.tagPayload}
-                        />
-                    </div>
-                </div>
-                }
-            </div>
-        );
-    }
+  render() {
+    const {
+      postNo,
+      isLoaded,
+      postPayload,
+      tagPayload,
+      menuActionType,
+    } = this.props;
+
+    if (_.isEmpty(postPayload) && isLoaded) return <NotFoundView isFail={true} />; // error
+    if (_.isEmpty(postPayload)) return <LoadingView />; // loading
+
+    const { post } = postPayload;
+    const { belongToMajor, belongToMinor, currentTags } = post[0];
+
+    // this.props.handleChangeMenu(postPayload.post[0], menuList);
+    // if (menuActionType === 'CHANGE_MENU') this.props.handleChangeMenuFinished();
+
+    return (
+      <div className="content">
+        <Helmet>
+          <meta property="og:url" content={`https://enhanced.kr/contentviewer/${postNo}`} />
+          <title>
+            {!post && `${post[0].title} :: ${blogTitle}`}
+          </title>
+        </Helmet>
+        <div className="content-body">
+          {belongToMajor !== 'Works' && (
+            <ContentViewSubtitle
+              tagPayload={tagPayload}
+              currentTags={currentTags}
+              belongToMinor={belongToMinor}
+            />
+          )}
+          <div className="content-view-wrapper">
+            <ContentViewContent
+              post={post[0]}
+              belongToMajor={belongToMajor}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ContentView = connect(
-    (state) => ({
-        post: state.posts.postPayload.post,
-        tagPayload: state.posts.tagPayload,
-        menuActionType: state.menus.menuActionType,
-        isLoaded: state.posts.isLoaded,
-    }),
-    (dispatch) => ({
-        handleFetchPost: async (url, postNo) => {
-            const postPayload = await dispatch(actions.fetchPost(url, postNo)).postPayload;
-            if(isEmpty(postPayload.post))  // when the post is not found
-                return dispatch(actions.fetchSuccess(postPayload));
-            const tagPayload = await dispatch(actions.fetchTags('/tags', postPayload.post[0].belongToMinor)).tagPayload;
-            dispatch(actions.fetchSuccess(postPayload, tagPayload));
-        },
-        handleChangeMenu: (post, menuList) => {
-            const belongToMajor = post[0].belongToMajor;
-            menuList.map((menu, idx) => {
-                if (menu.title === belongToMajor) {
-                    dispatch(actions.changeMenu(idx));
-                }
-            });
-        },
-        handleChangeMenuFinished: () => dispatch(actions.changeMenuFinished()),
-    }),
+ContentView.propTypes = {
+  postNo: PropTypes.number,
+  postPayload: PropTypes.objectOf({
+    post: [{
+      postNo: PropTypes.number,
+      title: PropTypes.string,
+      dateCreated: PropTypes.instanceOf(Date),
+      dateUpdated: PropTypes.instanceOf(Date),
+      content: PropTypes.string,
+      tags: PropTypes.array,
+      belongToMajor: PropTypes.string,
+      belongToMinor: PropTypes.string,
+    }],
+  }),
+  tagPayload: PropTypes.arrayOf({
+    tagName: PropTypes.string,
+    belongToMinor: PropTypes.string,
+    postList: PropTypes.array,
+  }),
+  menuActionType: PropTypes.string,
+  isLoaded: PropTypes.bool,
+};
+
+ContentView.defaultProps = {
+  postNo: -1,
+  postPayload: {},
+  tagPayload: [],
+  menuActionType: 'CHANGE_MENU_FINISHED',
+  isLoaded: true,
+};
+
+export default connect(
+  state => ({
+    postPayload: state.posts.postPayload,
+    tagPayload: state.posts.tagPayload,
+    menuActionType: state.menus.menuActionType,
+    isLoaded: state.posts.isLoaded,
+  }),
+  dispatch => ({
+    handleFetchPost: async (url, postNo) => {
+      const postPayload = await dispatch(actions.fetchPost(url, postNo)).postPayload;
+      if (!postPayload.post) { // when the post is not found
+        return dispatch(actions.fetchSuccess(postPayload));
+      }
+      const tagPayload = await dispatch(actions.fetchTags('/tags', postPayload.post[0].belongToMinor)).tagPayload;
+      dispatch(actions.fetchSuccess(postPayload, tagPayload));
+    },
+    handleChangeMenu: (post, menuList) => {
+      const belongToMajor = post.belongToMajor;
+      menuList.map((menu, idx) => {
+        if (menu.title === belongToMajor) {
+          dispatch(actions.changeMenu(idx));
+        }
+      });
+    },
+    handleChangeMenuFinished: () => dispatch(actions.changeMenuFinished()),
+  }),
 )(ContentView);
--- client/components/content/post-viewer/ContentViewContent.js
+++ client/components/content/post-viewer/ContentViewContent.js
@@ -1,55 +1,57 @@
-import React, {Component} from 'react';
-import {highlightCode, mdConverter} from "../../../../utils/mdModifier";
-import {decapitalizeFirstLetter} from "../../../../utils/stringModifier";
-import FacebookProvider, {Comments} from 'react-facebook';
-import {https2http} from "../../../../utils/urlModifier";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { highlightCode, mdConverter } from '../../../../utils/mdModifier';
+import { decapitalizeFirstLetter } from '../../../../utils/stringModifier';
 
-class ContentViewContent extends Component {
-    render() {
-        let url = https2http(window.location.href);
-        const renderContents = (post) => {
-            const postTitle = post[0].title;
-            const postContent = mdConverter(post[0].content);
-            const tags = post[0].tags;
-            const dateCreated = new Date(post[0].dateCreated);
-            const dateInFormat = dateCreated.getFullYear() + "년 " + (dateCreated.getMonth() + 1) + "월 " + dateCreated.getDate() + "일";
+const ContentViewContent = (props) => {
+  const renderTags = tags => tags.map(tag => <span>{` #${tag}`}</span>);
+  const {
+    post,
+    belongToMajor,
+  } = props;
+  const {
+    title,
+    content,
+    tags,
+    dateCreated,
+  } = post;
+  const convertedContent = mdConverter(content);
+  const dateRaw = new Date(dateCreated);
+  const dateInFormat = `${dateRaw.getFullYear()}년 ${dateRaw.getMonth() + 1}월 ${dateRaw.getDate()}일`;
 
-            const renderTags = (tags) => {
-                return tags.map((tag) => {
-                    return <span>{" #" + tag}</span>;
-                });
-            };
+  return (
+    <div className={['content-view', decapitalizeFirstLetter(belongToMajor)].join(' ')}>
+      <div ref={element => highlightCode(element)}>
+        <div className="post-title-wrapper">
+          <h1>{title}</h1>
+          <div className="post-meta-info">
+            <i className="far fa-calendar-alt"><span>{` ${dateInFormat}`}</span></i>
+            <i className="fas fa-tags">{renderTags(tags)}</i>
+          </div>
+        </div>
+        {convertedContent}
+      </div>
+    </div>
+  );
+};
 
-            return (
-                <div className={["content-view", decapitalizeFirstLetter(this.props.belongToMajor)].join(' ')}>
-                    <div ref={element => highlightCode(element)}>
-                        <div className="post-title-wrapper">
-                            <h1>{postTitle}</h1>
-                            <div className="post-meta-info">
-                                <i className="far fa-calendar-alt"><span>{" " + dateInFormat}</span></i>
-                                <i className="fas fa-tags">{renderTags(tags)}</i>
-                            </div>
-                        </div>
-                        {postContent}
-                    </div>
-                    <div className="fb-comments-wrapper">
-                        <FacebookProvider appId="1662680190479239" language="ko_KR">
-                            <Comments
-                                className="fb-comments"
-                                href={url}
-                                width="100%"
-                                numPosts={10}
-                            />
-                        </FacebookProvider>
-                    </div>
-                </div>
-            );
-        };
+ContentViewContent.propTypes = {
+  post: PropTypes.objectOf({
+    postNo: PropTypes.number,
+    title: PropTypes.string,
+    dateCreated: PropTypes.instanceOf(Date),
+    dateUpdated: PropTypes.instanceOf(Date),
+    content: PropTypes.string,
+    tags: PropTypes.array,
+    belongToMajor: PropTypes.string,
+    belongToMinor: PropTypes.string,
+  }),
+  belongToMajor: PropTypes.string,
+};
 
-        return (
-            renderContents(this.props.post)
-        );
-    }
-}
+ContentViewContent.defaultProps = {
+  post: {},
+  belongToMajor: '',
+};
 
 export default ContentViewContent;
