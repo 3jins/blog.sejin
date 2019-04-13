ebf0a4b0a04ea3ad447e7359cb426e2b2c0871e5
Sejin Jeon <jinaidy93@gmail.com>
2019-04-13 16:10:37 +0900

    Refactor format of PageView.js

--- client/components/content/PageView.js
+++ client/components/content/PageView.js
@@ -1,66 +1,72 @@
-import React, {Component} from 'react';
-import {getParameterByName} from "../../../utils/stringModifier";
-import {isEmpty} from "../../../utils/nullChecker";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getParameterByName } from '../../../utils/stringModifier';
+import { isEmpty } from '../../../utils/nullChecker';
 
-class PageView extends Component {
-    render() {
-        const page = isNaN(parseInt(this.props.page)) ? 1 : parseInt(this.props.page);
-        const numPosts = this.props.numPosts;
-        const pageScale = this.props.pageScale;
+const PageView = (props) => {
+  const { page, numPosts, pageScale } = props;
+  const isPageNeeded = numPosts > 10;
+  if (!isPageNeeded) return null;
 
-        const isPageNeeded = numPosts > 10;
+  const pages = [];
+  const pageControlButtons = {
+    prevBundle: '', prev: '', next: '', nextBundle: '',
+  };
+  const numPageBundle = 10;
+  const pageStart = Math.floor(page / pageScale) * pageScale + 1;
+  const finalPage = Math.floor(numPosts / pageScale) + 1;
+  let isLast = false;
 
-        const pages = [];
-        const otherButtons = {
-            prevBundle: "", prev: "", next: "", nextBundle: "",
-        };
-        const numPageBundle = 10;
-        const pageStart = Math.floor(page / pageScale) * pageScale + 1;
-        const finalPage = Math.floor(numPosts / pageScale) + 1;
-        let isLast = false;
+  const currentTag = getParameterByName('tag');
+  const urlQueryBase = isEmpty(currentTag) ? '?page=' : `?tag=${currentTag}&page=`;
+  for (let i = 0; i < numPageBundle; i++) {
+    if (pageStart + i > finalPage) {
+      isLast = true;
+      break;
+    }
+    if (pageStart + i === page) {
+      pages[i] = <span>{pageStart + i}</span>;
+    } else {
+      pages[i] = <a href={urlQueryBase + (pageStart + i)}>{pageStart + i}</a>;
+    }
+  }
+  if (page > 1) {
+    pageControlButtons.prev = <a href={urlQueryBase + (page - 1)}>{'<'}</a>;
+  }
+  if (page > 10) {
+    pageControlButtons.prevBundle = <a href={urlQueryBase + (page - numPageBundle)}>{'<<'}</a>;
+  }
+  if (page < finalPage) {
+    pageControlButtons.next = <a href={urlQueryBase + (page + 1)}>{'>'}</a>;
+  }
+  if (!isLast) {
+    pageControlButtons.nextBundle = <a href={urlQueryBase + (page + numPageBundle)}>{'>>'}</a>;
+  }
 
-        if (!isPageNeeded) return null;
+  return (
+    <div className="pages">
+      {pageControlButtons.prevBundle}
+      {pageControlButtons.prev}
+      {pages}
+      {pageControlButtons.next}
+      {pageControlButtons.nextBundle}
+      <p>
+        Current page: {page} of <a href={urlQueryBase + finalPage}>{finalPage}</a>
+      </p>
+    </div>
+  );
+};
 
-        const currentTag = getParameterByName('tag');
-        const urlQueryBase = isEmpty(currentTag) ? "?page=" : "?tag=" + currentTag + "&page=";
-        for (let i = 0; i < numPageBundle; i++) {
-            if (pageStart + i > finalPage) {
-                isLast = true;
-                break;
-            }
-            if (pageStart + i === page) {
-                pages[i] = <span>{pageStart + i}</span>;
-            }
-            else {
-                pages[i] = <a href={urlQueryBase + (pageStart + i)}>{pageStart + i}</a>;
-            }
-        }
-        if (page > 1) {
-            otherButtons.prev = <a href={urlQueryBase + (page - 1)}>{"<"}</a>;
-        }
-        if (page > 10) {
-            otherButtons.prevBundle = <a href={urlQueryBase + (page - numPageBundle)}>{"<<"}</a>;
-        }
-        if (page < finalPage) {
-            otherButtons.next = <a href={urlQueryBase + (page + 1)}>{">"}</a>;
-        }
-        if (!isLast) {
-            otherButtons.nextBundle = <a href={urlQueryBase + (page + numPageBundle)}>{">>"}</a>;
-        }
+PageView.propTypes = {
+  page: PropTypes.number,
+  numPosts: PropTypes.number,
+  pageScale: PropTypes.number,
+};
 
-        return (
-            <div className="pages">
-                {otherButtons.prevBundle}
-                {otherButtons.prev}
-                {pages}
-                {otherButtons.next}
-                {otherButtons.nextBundle}
-                <p>
-                    Current page: {page} of <a href={urlQueryBase + finalPage}>{finalPage}</a>
-                </p>
-            </div>
-        );
-    };
-}
+PageView.defaultProps = {
+  page: 1,
+  numPosts: 0,
+  pageScale: 10,
+};
 
 export default PageView;
