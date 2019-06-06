8491070e2f5215235cc14e1127a64fc16bda4e03
Sejin Jeon <jinaidy93@gmail.com>
2019-06-07 04:27:24 +0900

    Fix the format of sitemap.xml

--- server/scripts/buildSitemap.js
+++ server/scripts/buildSitemap.js
@@ -6,42 +6,43 @@ import * as PostDao from '../mongoDB/dao/PostDao';
 
 const makeUrlListFromData = (postList) => {
   const siteBaseUrl = 'https://enhanced.kr';
+  const postNoThreshold = 3;
   const baseUrlObjList = [
     {
       url: {
         loc: siteBaseUrl,
-        lastmod: new Date().toISOString(),
+        lastmod: new Date().toISOString().split('T')[0],
         priority: 1.00,
       },
     },
     {
       url: {
         loc: `${siteBaseUrl}/nav/About`,
-        lastmod: new Date().toISOString(),
+        lastmod: new Date().toISOString().split('T')[0],
         priority: 9 * 9 * 9 / 1000,
       },
     },
     {
       url: {
         loc: `${siteBaseUrl}/nav/Works`,
-        lastmod: new Date().toISOString(),
+        lastmod: new Date().toISOString().split('T')[0],
         priority: 9 * 9 * 9 / 1000,
       },
     },
     {
       url: {
         loc: `${siteBaseUrl}/nav/Blog`,
-        lastmod: new Date().toISOString(),
+        lastmod: new Date().toISOString().split('T')[0],
         priority: 9 * 9 * 9 / 1000,
       },
     },
   ];
   const postUrlObjList = postList
-    .filter(post => post.postNo >= 3) // Pass data for 'About'
+    .filter(post => post.postNo >= postNoThreshold) // Pass data for 'About'
     .map(post => ({
       url: {
         loc: [siteBaseUrl, 'postviewer', post.postNo].join('/'),
-        lastmod: post.dateUpdated,
+        lastmod: post.dateUpdated.toISOString().split('T')[0],
         priority: 0.9,
       },
     }));
@@ -61,7 +62,7 @@ const saveObjectAsXML = (sitemapPath, sitemapFileName, sitemapContent) => fs.wri
 );
 
 export default () => PostDao.findAllPosts().then((postList) => {
-  const rawSitemapOpener = '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="https://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://www.sitemaps.org/schemas/sitemap/0.9 https://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+  const rawSitemapOpener = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="https://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://www.sitemaps.org/schemas/sitemap/0.9 https://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
   const urlList = makeUrlListFromData(postList);
   const rawSitemapCloser = '</urlset>';
 
